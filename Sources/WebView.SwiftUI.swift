c8c557ca540f4553094106b65def623d9531cf10
Ivan Kh <ihvorostinin@gmail.com>
2026-04-10 15:43:17 +0700

    More WebView wrappers

--- Sources/WebView.SwiftUI.swift
+++ Sources/WebView.SwiftUI.swift
@@ -3,7 +3,7 @@
 import SwiftUI
 import WebKit
 
-public struct WebView: ViewRepresentable9 {
+public struct WebViewWrapper: ViewRepresentable9 {
     let webView: WKWebView
     
     public init(_ webView: WKWebView) {
@@ -22,5 +22,30 @@ public struct WebView: ViewRepresentable9 {
     }
     
     public func updateNSView(_ uiView: WKWebView, context: Context) {
+    }
+}
+
+public struct WebView: ViewRepresentable9 {
+    let url: URL
+    
+    public init(_ url: URL) {
+        self.url = url
+    }
+    
+    public func makeUIView(context: Context) -> WKWebView {
+        WKWebView()
+    }
+    
+    public func makeNSView(context: Context) -> WKWebView {
+        makeUIView(context: context)
+    }
+    
+    public func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard uiView.url != url else { return }
+        uiView.load(URLRequest(url: url))
+    }
+    
+    public func updateNSView(_ uiView: WKWebView, context: Context) {
+        updateUIView(uiView, context: context)
     }
 }
